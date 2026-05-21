d8f57fd33b6df17d185bfbea37b5a0e9a43c93f0
Casper Christensen <Casper@perb.dk>
2026-05-21 10:44:43 +0200

    Wishlist oprettet

---
+++ 2SemesterOpgave/Models/WishList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2SemesterOpgave.Models
+{
+    public class WishList
+    {
+        public Article article;
+        public User user;
+        public DateTime Createdadd;
+
+        public WishList(Article article, User user)
+        {
+            this.article = article;
+            this.user = user;
+            this.Createdadd = DateTime.Now;
+        }
+    }
+}
